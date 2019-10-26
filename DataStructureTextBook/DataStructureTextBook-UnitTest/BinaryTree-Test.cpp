810b91b444c93ff3724dd32861bde4f07c49edb2
TempeBrennan <425751342@qq.com>
2019-10-26 16:33:17 +0800

    Add Tree Order

--- DataStructureTextBook/DataStructureTextBook-UnitTest/BinaryTree-Test.cpp
+++ DataStructureTextBook/DataStructureTextBook-UnitTest/BinaryTree-Test.cpp
@@ -11,25 +11,78 @@ namespace DataStructureTextBookUnitTest
 	{
 	public:
 
+		BiTree CreateTree01() {
+			BiTree tr1 = CreateTNode('A');
+			BiTree tr2 = CreateTNode('B');
+			BiTree tr3 = CreateTNode('C');
+			BiTree tr4 = CreateTNode('D');
+			BiTree tr5 = CreateTNode('E');
+			BiTree tr6 = CreateTNode('F');
+			BiTree tr7 = CreateTNode('G');
+			BiTree tr8 = CreateTNode('H');
+			tr1->LChild = tr2;
+			tr1->RChild = tr3;
+			tr2->LChild = tr4;
+			tr3->LChild = tr5;
+			tr3->RChild = tr6;
+			tr4->RChild = tr7;
+			tr5->RChild = tr8;
+			return tr1;
+		}
+
+		BiTree CreateTree02() {
+			BiTree tr1 = CreateTNode('A');
+			BiTree tr2 = CreateTNode('B');
+			BiTree tr3 = CreateTNode('C');
+			BiTree tr4 = CreateTNode('D');
+			BiTree tr5 = CreateTNode('E');
+			BiTree tr6 = CreateTNode('F');
+			BiTree tr7 = CreateTNode('G');
+			BiTree tr8 = CreateTNode('H');
+			tr1->LChild = tr2;
+			tr1->RChild = tr3;
+			tr2->RChild = tr4;
+			tr3->LChild = tr5;
+			tr3->RChild = tr6;
+			tr4->LChild = tr7;
+			tr6->LChild = tr8;
+			return tr1;
+		}
+
 		TEST_METHOD(TestMethod1)
 		{
-			BiTree root = CreateTNode('A');
-			BiTree left = CreateTNode('B');
-			BiTree right = CreateTNode('C');
-			root->LChild = left;
-			root->RChild = right;
-			Assert::AreEqual('C', root->RChild->data);
+			BiTree tree = CreateTree01();
+			PreOrder(tree);
 		}
 
 		TEST_METHOD(TestMethod2)
 		{
-
+			BiTree tree = CreateTree01();
+			InOrder(tree);
 		}
 
 		TEST_METHOD(TestMethod3)
 		{
-
+			BiTree tree = CreateTree01();
+			PostOrder(tree);
 		}
 
+		TEST_METHOD(TestMethod4)
+		{
+			BiTree tree = CreateTree02();
+			PreOrder(tree);
+		}
+
+		TEST_METHOD(TestMethod5)
+		{
+			BiTree tree = CreateTree02();
+			InOrder(tree);
+		}
+
+		TEST_METHOD(TestMethod6)
+		{
+			BiTree tree = CreateTree02();
+			PostOrder(tree);
+		}
 	};
 }
--- DataStructureTextBook/DataStructureTextBook/BinaryTree.c
+++ DataStructureTextBook/DataStructureTextBook/BinaryTree.c
@@ -9,3 +9,37 @@ BiTree CreateTNode(char data) {
 	root->RChild = NULL;
 	return root;
 }
+
+void VisitTNode(char data) {
+	printf(" %c ", data);
+}
+
+void PreOrder(BiTree root) {
+	if (!root) {
+		return;
+	}
+
+	VisitTNode(root->data);
+	PreOrder(root->LChild);
+	PreOrder(root->RChild);
+}
+
+void InOrder(BiTree root) {
+	if (!root) {
+		return;
+	}
+
+	PreOrder(root->LChild);
+	VisitTNode(root->data);
+	PreOrder(root->RChild);
+}
+
+void PostOrder(BiTree root) {
+	if (!root) {
+		return;
+	}
+
+	PreOrder(root->LChild);
+	PreOrder(root->RChild);
+	VisitTNode(root->data);
+}
